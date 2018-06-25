869968f57c15805e84d9c371310d494f31b19e05
vnadar <vnadar@webuy.com>
2018-06-25 12:22:04 +0530

    Project files

---
+++ chapter2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace chapter2
+{
+    class Program
+    {
+		double maxTime;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Doubler(2, Result);
+			Doubler(3, Result);
+		    Doubler(10, Result);
+			Console.ReadLine();
+        }
+    
+	
+	private static void Doubler(int v, Action<string, int> callback) {
+		Console.WriteLine("Worker thread in Doubler " + Thread.CurrentThread.ManagedThreadId);
+		//var waitTime = Math.Floor(Math()*(maxTime+1));
+		if (v%2 != 0) {
+			Task.Run(() => callback("Odd input", 0));			
+		}
+		else {
+		    Task.Run(() => callback(null, v * 2));	
+		}
+	}
+
+	private static void Result(string err, int results){
+		Console.WriteLine("Worker thread in handleResults " + Thread.CurrentThread.ManagedThreadId);
+		if(err != null){
+		   Console.WriteLine("Error: " + err);
+		}
+		else {
+		   Console.WriteLine("The results are: " + results);
+		}
+	}
+
+}
+	
+	
+}
+
+
